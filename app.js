eca6d6decddb20170635f02554eb1f5de0c1ab47
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-03 13:11:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// chose a color for an alien as you 
+// did in exercise 25, and write an if-else chain.
+var alien_color = "green";
+// if the alien's color is green, print a statements that
+// the player just earned 5 points for shooting the alien.
+if (alien_color == "green") {
+    console.log("the player just earned 5 points. for shooting the alien");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points. for shooting the alien");
+}
